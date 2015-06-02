4f6c63ec42c4031497a42d48245b08a5e715dad7
rsevil <rodrigo.sevil@gmail.com>
2015-06-02 20:08:01 -0300

    Zigzag + correcciones en el uml

--- src/Modelo/Zigzag.java
+++ src/Modelo/Zigzag.java
@@ -1,12 +1,45 @@
 package Modelo;
 
 public class Zigzag extends Movimiento {
+	
+	private int frames;
+	private boolean derecha;
+	private boolean izquierda;
+	
 	public Zigzag(int anchoPantalla, int altoPantalla){
 		super(anchoPantalla, altoPantalla);	
+		this.frames = 0;
+		this.derecha = true;
+		this.izquierda = false;
 	}
 	
 	public Posicion calcularPosicion(float vx, float vy, float deltaTiempo, int diametro, Posicion posicion) {
-		//TODO:Calcular posicion nueva
-		return posicion;
+		this.frames++;
+		float xf = posicion.obtenerX() + diametro;
+		if (this.frames == 180 && xf < this.anchoPantalla){
+			if(this.derecha){
+				this.derecha = false;
+				this.izquierda = true;
+			}else if (this.izquierda){
+				this.izquierda = false;
+				this.derecha = true;
+			}
+			this.frames = 0;
+		}else{
+			if (this.derecha && xf >= anchoPantalla){
+				this.derecha = false;
+				this.izquierda = true;
+			}else if (this.izquierda && xf <= anchoPantalla){
+				this.derecha = false;
+				this.izquierda = true;
+			}
+		}
+		
+		if (izquierda)
+			vx = -vx;
+		
+		return new Posicion(
+				limites(posicion.obtenerX() + vx * deltaTiempo, 0, this.anchoPantalla - diametro),
+				limites(posicion.obtenerY() + vy * deltaTiempo, 0, this.altoPantalla - diametro));
 	}
 }
