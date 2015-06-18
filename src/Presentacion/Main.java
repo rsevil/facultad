c8039b69de0f69b3f72c432e02562a7d50e93270
rsevil <rodrigo.sevil@gmail.com>
2015-06-18 11:21:31 -0300

    MenuPrincipal.java

--- src/Presentacion/Main.java
+++ src/Presentacion/Main.java
@@ -1,15 +1,13 @@
 package Presentacion;
 
-//import Modelo.*;
+import Presentacion.MenuPrincipal;
 
 public class Main {
 
 	public static void main(String[] args) {
-//		Juego j = new Juego(640, 480);
-//		 
-//		Partida p = j.nuevaPartida();
-//		
-//		p.elegirNombre("Rodrigo");
+		// TODO Auto-generated method stub
+		MenuPrincipal miMenuPrincipal= new MenuPrincipal();
+		miMenuPrincipal.setVisible(true);
 	}
 
 }
